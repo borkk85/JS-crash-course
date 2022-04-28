059bb6aae457f7c8587ef64c5e0419ba67431ed5
Borko <bor.kos.mk@gmail.com>
2022-04-28 14:35:25 +0200

    commit on main

---
+++ main.js
@@ -0,0 +1,39 @@
+// LOGGING OUTPUT
+// alert('Hello World'); // Do not use for debugging. Stops script and only strings
+// console.log('Hello World');
+// console.error('This is an error');
+// console.warn('This is a warning');
+
+// var, let(re-assigned), const(not-changed)
+
+// const age = 30;
+
+// age = 31
+
+// console.log(age)
+
+
+// DATA TYPES - String, Number, Boolean, null, undefined
+
+// const name = 'John';
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined; 
+// let z;
+
+// console.log(typeof x) 
+
+//Strings
+
+const name = 'John';
+const age = 30;
+
+// Concatenation
+
+console.log('My name is ' + name + ' and i am ' + age);
+//Template String
+
+console.log(`My name is ${name} and i am ${age}`)
+
